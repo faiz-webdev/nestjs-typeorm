cc26b8a8a51e40a1b96e938fd0ce4ac183428085
faizttn <faijuddin@tothenew.com>
2023-12-18 12:29:15 +0530

    connect database with confiservoce and separte dbmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,26 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './typeorm/entities/User';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CompanyModule } from './company/company.module';
+import { DataSource } from 'typeorm';
+import { DBModule } from './db.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({}),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      database: 'nestjs_typeorm',
-      password: 'root',
-      entities: [User],
-      synchronize: true,
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   database: 'nestjs_typeorm',
+    //   password: 'root',
+    //   entities: [User],
+    //   synchronize: true,
+    //   autoLoadEntities: true,
+    // }),
+    DBModule,
     UsersModule,
     CompanyModule,
   ],
---
+++ src/db.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/entities/User';
+import { DataSource } from 'typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        database: configService.get<string>('DB_DATABASENAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        entities: [User],
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+      dataSourceFactory: async (options) => {
+        const dataSource = await new DataSource(options).initialize();
+        return dataSource;
+      },
+    }),
+  ],
+})
+export class DBModule {}
